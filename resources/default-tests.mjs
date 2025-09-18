1a07fff28c82e73daae18f9363d2780a62300365
Daniel Lehmann <dlehmann@google.com>
2025-09-18 17:48:50 +0200

    Add an empty workload as a template
    Change-Id: Ib6ccf41bb7e24361180e80a6352c2d6c5f6076fe

--- resources/default-tests.mjs
+++ resources/default-tests.mjs
@@ -1,16 +1,21 @@
 import { BenchmarkTestStep } from "./benchmark-runner.mjs";
-import { getTodoText, defaultLanguage } from "./shared/translations.mjs";
-import { numberOfItemsToAdd } from "./shared/todomvc-utils.mjs";
 
 export const defaultSuites = [
+    // {
+    //     name: "TodoMVC-WebComponents-PostMessage",
+    //     url: "resources/todomvc/vanilla-examples/javascript-web-components/dist/index.html",
+    //     tags: ["default", "todomvc", "webcomponents"],
+    //     async prepare() {},
+    //     type: "remote",
+    //     /* config: {
+    //         name: "default", // optional param to target non-default tests locally
+    //     }, */
+    // },
     {
-        name: "TodoMVC-WebComponents-PostMessage",
-        url: "resources/todomvc/vanilla-examples/javascript-web-components/dist/index.html",
-        tags: ["default", "todomvc", "webcomponents"],
+        name: "Empty-PostMessage",
+        url: "resources/empty/dist/index.html",
+        tags: ["default", "empty"],
         async prepare() {},
         type: "remote",
-        /* config: {
-            name: "default", // optional param to target non-default tests locally
-        }, */
-    },
+    }
 ];
