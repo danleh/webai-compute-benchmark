a82083e2a1b2a04501266f37939165f32979be52
Rezvan Mahdavi Hezaveh <31081679+rmahdav@users.noreply.github.com>
2026-03-23 18:35:18 +0000

    Add first partner workload: use Xenova/mobileclip_s0 for zero-shot image classification (#121)
    The MobileClip model ([suggested by a
    partner](https://docs.google.com/spreadsheets/d/1yC_5o4pRj4ARe5xaeppvHulww-pJY4L0VpAJ8pypVbc/edit?resourcekey=0-HfmoScK-js53TujSZhYkhg&gid=0#gid=0))
    is smaller than the model we used in our zero-shot image classification
    (Marqo/marqo-fashionSigLIP), and also looks like it is more suitable for
    on-device real time applications.
    
    The results also improved. Previously, the measured time for one
    inference was ~460ms (wasm) and ~600ms (webgpu). After using the
    MobileClip model, the measured time for a single inference is ~135ms
    (wasm) and ~39ms (webgpu).

--- resources/transformers-js/licenses/NOTICE.md
+++ resources/transformers-js/licenses/NOTICE.md
@@ -7,5 +7,5 @@
     * [Xenova/modnet](https://huggingface.co/Xenova/modnet): licensed under the Apache License, Version 2.0. The full text is available in [LICENSE.apache2](LICENSE.apache2).
     * [mixedbread-ai/mxbai-rerank-base-v1](https://huggingface.co/mixedbread-ai/mxbai-rerank-base-v1): licensed under the Apache License, Version 2.0. The full text is available in [LICENSE.apache2](LICENSE.apache2).
     * [AdamCodd/vit-base-nsfw-detector](https://huggingface.co/AdamCodd/vit-base-nsfw-detector): licensed under the Apache License, Version 2.0. The full text is available in [LICENSE.apache2](LICENSE.apache2).
-    * [Marqo/marqo-fashionSigLIP](https://huggingface.co/Marqo/marqo-fashionSigLIP): licensed under the Apache License, Version 2.0. The full text is available in [LICENSE.apache2](LICENSE.apache2).
+    * [Xenova/mobileclip_s0](https://huggingface.co/Xenova/mobileclip_s0): licensed under the Apple MIT License. The full text is available in [LICENSE.apple](LICENSE.apple).
     * [onnx-community/Kokoro-82M-v1.0-ONNX](https://huggingface.co/onnx-community/Kokoro-82M-v1.0-ONNX): licensed under the Apache License, Version 2.0. The full text is available in [LICENSE.apache2](LICENSE.apache2).
--- resources/transformers-js/src/index.mjs
+++ resources/transformers-js/src/index.mjs
@@ -1,7 +1,7 @@
 import { BenchmarkConnector } from "speedometer-utils/benchmark.mjs";
 import { createSubIteratedSuite } from "speedometer-utils/helpers.mjs";
 import { params } from "speedometer-utils/params.mjs";
-import { pipeline, env, dot, read_audio, AutoTokenizer, AutoModelForSequenceClassification, SiglipVisionModel, AutoImageProcessor, RawImage, SiglipTextModel, softmax } from '@huggingface/transformers';
+import { pipeline, env, dot, read_audio, AutoTokenizer, AutoModelForSequenceClassification, AutoProcessor, RawImage, CLIPTextModelWithProjection, CLIPVisionModelWithProjection, softmax } from '@huggingface/transformers';
 import { KokoroTTS } from "kokoro-js";
 import jfkAudio from '../../media/jfk_1962_0912_spaceeffort.wav';
 import imageWithBackground from '../../media/image.jpg';
@@ -251,7 +251,7 @@ class ImageClassification {
   }
 }
 
-/*--------- Zero-shot image classification workload using Marqo/marqo-fashionSigLIP ---------*/
+/*--------- Zero-shot image classification workload using Xenova/mobileclip_s0 ---------*/
 
 class ZeroShotImageClassification {
   constructor(device) {
@@ -262,14 +262,14 @@ class ZeroShotImageClassification {
   async init() {
     document.getElementById('device').textContent = this.device;
     document.getElementById('workload').textContent = "zero-shot image classification";
-    document.getElementById('input').textContent = `Classifying an image against the following labels: ${JSON.stringify(this.texts)}`;
+    document.getElementById('input').textContent = `Classifying a local image against the following labels: ${JSON.stringify(this.texts)}`;
     
-    const model_id = "Marqo/marqo-fashionSigLIP";
+    const model_id = "Xenova/mobileclip_s0";
 
-    this.tokenizer = await AutoTokenizer.from_pretrained(model_id, { device: this.device});
-    this.text_model = await SiglipTextModel.from_pretrained(model_id, { device: this.device, dtype: "bnb4" });
-    this.processor = await AutoImageProcessor.from_pretrained(model_id, { device: this.device});
-    this.vision_model = await SiglipVisionModel.from_pretrained(model_id, { device: this.device, dtype: "bnb4" });
+    this.tokenizer = await AutoTokenizer.from_pretrained(model_id, { device: this.device });
+    this.processor = await AutoProcessor.from_pretrained(model_id, { device: this.device });
+    this.text_model = await CLIPTextModelWithProjection.from_pretrained(model_id, { device: this.device, dtype: "q4" });
+    this.vision_model = await CLIPVisionModelWithProjection.from_pretrained(model_id, { device: this.device, dtype: "q4" });
 
     this.image = await RawImage.read(this.imageURL);
   }
